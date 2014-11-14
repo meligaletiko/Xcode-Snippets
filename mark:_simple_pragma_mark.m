ec726aa22b2b40102a6d74f297b944ac5fc710e8
Luís Portela Afonso <meligaletiko@gmail.com>
2014-11-14 21:57:36 +0000

    Default Xcode Snippets

---
+++ mark:_simple_pragma_mark.m
@@ -0,0 +1,10 @@
+// Mark: Simple pragma mark
+// Just creates a pragma mark with a line
+//
+// IDECodeSnippetCompletionPrefix: mark
+// IDECodeSnippetCompletionScopes: [ClassInterfaceMethods,ClassImplementation]
+// IDECodeSnippetIdentifier: F119F278-AD62-44DE-92B0-F3DBFFB5C7FE
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+#pragma mark - <#mark#>
