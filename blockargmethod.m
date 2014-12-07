87bf5c0e6e7f359aa975dd3e193dddd9e0937c93
Luís Portela Afonso <meligaletiko@gmail.com>
2014-12-07 19:05:08 +0000

    Added shorcut and scope
    	* async.m
    	* blockargmethod.m

--- blockargmethod.m
+++ blockargmethod.m
@@ -1,8 +1,8 @@
 // Block argument to a method call
 // Block as an argument to a method call
 //
-// IDECodeSnippetCompletionPrefix: block_arg_method snippet
-// IDECodeSnippetCompletionScopes: [CodeBlock]
+// Shortcut: block_arg_method snippet
+// Scope: CodeBlock, ClassInterfaceVariables
 // IDECodeSnippetIdentifier: C43EF7F5-6400-4E0A-9B2D-91DBD480C04D
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
